bce76e5d790133c444d5af9c3a6cedcb7bd4eb97
U-FSOFT.FPT.VN\NamBV2 <NamBV2@101NamBV2.fsoft.fpt.vn>
2015-03-09 18:00:06 +0700

    done step 0,1,2 in guideline phonecat App include bootstrapping, static template,angular templates

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,11 @@
 'use strict';
 
 /* Controllers */
+var phonecatApp = angular.module('phonecatApp', []);
+phonecatApp.controller('PhoneListCtrl', ['$scope', function ($scope) {
+	$scope.phones=[
+		{'name':'Galaxy','snippet':'Fast just got faster with Nexus S.'},
+		{'name': 'Motorola XOOM™ with Wi-Fi','snippet': 'The Next, Next Generation tablet.'},
+    	{'name': 'MOTOROLA XOOM™','snippet': 'The Next, Next Generation tablet.'}
+	];
+}]);
